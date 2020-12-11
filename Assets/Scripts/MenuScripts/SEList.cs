5d6894e558375baf11f4f6312f15f080a771799c
Saku-3939 <54201388+Saku-3939@users.noreply.github.com>
2020-12-11 15:55:19 +0900

    スコア画面の変更、一部表現の追加
    スコアにSEとアニメーションを追加、敵の玉に軌跡が残るよう変更

---
+++ Assets/Scripts/MenuScripts/SEList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEList : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] se;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ScoreSE()
+    {
+        Debug.Log("Active!");
+        audioSource.PlayOneShot(se[0]);
+    }
+}
